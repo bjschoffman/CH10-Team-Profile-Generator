982c7fb73e3909dc6e056450269d4c0660ffefc2
bjschoffman <bjschoffman@gmail.com>
2023-01-26 17:14:17 -0600

    updated code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const Manager = require("./lib/Manager")
-const inquirer = require("inquirer") 
-const createPage = require("./src/template")
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer"); 
+const createPage = require("./src/template");
 const fs = require("fs")
 let team = []
 
@@ -29,19 +31,58 @@ const addManager = () => {
             message: "What is the manager's office number?"
         }
     ]) 
-    .then(managerInput => {
-        // console.log(managerInput) 
+    .then(managerInput => { 
         const newManager = new Manager(
             team.length +1,
             managerInput.name,
-            
+            managerInput.email,
+            managerInput.id,
+            managerInput.officeNumber
         )
         team.push(newManager)
-        // console.log(team)
-        newHTML()
+        
     })
 }
 
+const addEngineer = () => {
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the engineer's name?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "what is the engineer's email?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the engineer's ID?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is the engineer's Github username?"
+        }
+    ]) 
+    .then(engineerInput => {
+        
+        const newEngineer = new Engineer(
+            team.length +1,
+            engineerInput.name,
+            engineerInput.email,
+            engineerInput.id,
+            engineerInput.github
+        )
+        
+        team.push(newEngineer)
+       
+        
+    })
+}
 
 const newHTML = () => {
     const pageHTML = createPage(team)
@@ -54,3 +95,4 @@ const newHTML = () => {
 
 
 addManager()
+addEngineer()
